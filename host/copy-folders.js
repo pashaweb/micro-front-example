e7b605440569ec65981f9c5763f9144af337fa8e
pavelk <pavel.k@selectmedia.com>
2022-09-26 13:40:11 +0200

    copy script update

--- host/copy-folders.js
+++ host/copy-folders.js
@@ -1,14 +1,58 @@
 // include fs-extra package
 import { copy } from 'fs-extra';
- 
-const source = '../grid/dist'
-const destination = './public/external/grid';
- console.log("Copying files from " + source + " to " + destination);
-// copy source folder to destination
-copy(source, destination, function (err) {
-    if (err){
-        console.log('An error occured while copying the folder.')
-        return console.error(err)
-    }
-    console.log('Copy completed!')
+//console.log("Current directory:", process.cwd());
+import path from 'path';
+import { cwd } from 'process';
+import fs from 'fs';
+
+
+const directoryName = path.basename(cwd());
+const parent = path.dirname(cwd());
+console.log(directoryName);
+console.log(parent);
+const copySettings = fs.readdirSync(parent, { withFileTypes: true })
+    .filter((item) => {
+        if (!item.isDirectory()) return false;
+        if (item.name.startsWith('.')) return false;
+        if (item.name === directoryName) return false;
+        return true;
+    })
+    .map((item) => {
+        return {
+            from: path.join(parent, item.name, 'dist'),
+            to: path.join(cwd(), 'public', 'external', item.name)
+        }
+    });
+
+console.log(copySettings);
+copySettings.forEach((item) => {
+    copy(item.from, item.to, (err) => {
+        if (err) {
+            console.error(err);
+        }else{
+            console.log("Copied", item.from, "to", item.to);
+        }
+    });
 });
+// const externalPaths = [
+//     {
+//         from: '../grid/dist',
+//         to: 'host/external-folder'
+//     },
+//     {
+//         from: 'host/external-folder-2',
+//         to: 'host/external-folder-2'
+//     }
+// ];
+// const source = '../grid/dist'
+// const destination = './public/external/grid';
+//  console.log("Copying files from " + source + " to " + destination);
+// // copy source folder to destination
+// function copyFiles(source, destination) {
+//     copy(source, destination, function (err) {
+//         if (err) {
+//             return console.error(err);
+//         }
+//         console.log("success!", `${source} copy to ${destination}`);
+//     });
+// }
